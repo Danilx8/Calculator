b3d96b44d818ce79ed30bebeee44def59cc1a014
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-29 17:14:11 +0700

    Added first methods and variables for the BigInt class, which is going to perform all the manipulations and store the numbers as char vectors

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,41 +7,134 @@
 ********************************/
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
 using namespace std;
 
-int main() {
-  int accumulatingVariable = 0;
-  int currentNumber;
-  char operation = '+';
+class BigInt {
+  private:
+    enum Sign {positive, negative};
+    Sign mySign;
+    vector<char> myDigits;
+    int myDigitsNumber; // = myNumDigits
 
-  while (operation != '=') {
-    cout << "enter value: ";
-    cin >> currentNumber;
+  public:
 
-    switch (operation) {
-      case '+':
-        accumulatingVariable += currentNumber;
-        break;
-
-      case '-':
-        accumulatingVariable -= currentNumber;
-        break;
-
-      case '*':
-        accumulatingVariable *= currentNumber;
-        break;
-      
-      case '/':
-        accumulatingVariable /= currentNumber;
-        break;
-          
-      default:
-        cout << "error with operator" << endl;
+  private:
+    void AddDigit (int value) {
+      if (myDigitsNumber >= myDigits.size()) {
+        myDigits.resize(myDigits.size() * 2);
+      }
+      myDigits[myDigitsNumber] = char('0' + value);
+      ++myDigitsNumber;
     }
-    cout << "--> " << accumulatingVariable << endl;
-    cout << "Enter operator (+ - * / (= to quit)): ";
-    cin >> operation;
-  }
-  cout << accumulatingVariable;
+    
+  
+  public:
+    BigInt() {
+      mySign(positive);
+      myDigits(1, '0');
+      myDigitsNumber(0);
+    }
+
+    BigInt(int number) {
+      mySign(positive);
+      myDigits(inputString.length(), '0');
+      myDigitsNumber(0);
+      
+      if (number < 0) {
+        mySign = negative;
+        number *= -1;
+      }
+
+      do {
+        AddDigit(number % 10);
+        number /= 10;
+      } while (number != 0);
+
+    }
+
+    BigInt(const string& inputString) {
+      mySign(positive);
+      myDigits(inputString.length(), '0');
+      myDigitsNumber(0);
+      int numberIndex;
+      int limit = 0;
+
+      if (inputString.size()) == 0) {
+        myDigits.resize(1);
+        AddDigit(0);
+        return;
+      }
+
+      if (s[0] == '-') {
+        mySign = negative;
+        limit = 1;
+      }
+
+      if (s[0] == '+') {
+        limit = 1;
+      }
+
+      for (numberIndex = inputString.size(); numberIndex >= limit; --k) {
+        AddDigit(s[k] - '0');
+      }
+    }
+    
+    istream& operator>> (istream& in, BigInt& number) {
+      string line;
+      in >> line;
+      number = BigInt(line);
+      return line;
+    }
+
+    /*void Print(ostream& out) {
+      int len = numDigits
+    }*/
+
+};
+
+int main() {
+  int num = 5;
+  BigInt();
+
+  /*
+    int accumulatingVariable = 0;
+    int currentNumber;
+    char operation = '+';
+
+
+    while (operation != '=') {
+      cout << "enter value: ";
+      cin >> currentNumber;
+
+      switch (operation) {
+        case '+':
+          accumulatingVariable += currentNumber;
+          break;
+
+        case '-':
+          accumulatingVariable -= currentNumber;
+          break;
+
+        case '*':
+          accumulatingVariable *= currentNumber;
+          break;
+
+        case '/':
+          accumulatingVariable /= currentNumber;
+          break;
+
+        default:
+          cout << "error with operator" << endl;
+      }
+      cout << "--> " << accumulatingVariable << endl;
+      cout << "Enter operator (+ - * / (= to quit)): ";
+      cin >> operation;
+    }
+    cout << accumulatingVariable;
+  */
   return 0;
 }
