a21bfbaf8c1fe98cd5ef81f33a090590b7f5e8da
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-30 18:20:36 +0700

    Added the parameters and method for input, output a nd initialization of BigInt

--- Calculator.cpp
+++ Calculator.cpp
@@ -18,7 +18,7 @@ class BigInt {
     enum Sign {positive, negative};
     Sign mySign;
     vector<char> myDigits;
-    int myDigitsNumber; // = myNumDigits
+    int myDigitsNumber; 
 
   public:
 
@@ -31,19 +31,66 @@ class BigInt {
       ++myDigitsNumber;
     }
     
-  
-  public:
-    BigInt() {
-      mySign(positive);
-      myDigits(1, '0');
-      myDigitsNumber(0);
-    }
-
-    BigInt(int number) {
-      mySign(positive);
-      myDigits(inputString.length(), '0');
-      myDigitsNumber(0);
+    string ToString() {
+      int numberIndex;
+      int length = myDigitsNumber;
+      string readyString;
       
+      if (IsNegative()) {
+        readyString = '-';
+      }
+      
+      for (numberIndex = length - 1; numberIndex >= 0; --numberIndex) {
+        readyString += char('0' + GetDigit(numberIndex));
+      }
+      
+      return readyString;
+    }
+    
+    int GetDigit(int numberIndex) {
+      if (numberIndex >= 0 && numberIndex < myDigitsNumber){
+        return myDigits[numberIndex] - '0';
+      }
+      return 0;
+    }
+    
+    bool IsNegative() {
+      return mySign == negative;
+    }  
+    
+    bool IsPositive() {
+      return mySign == positive;
+    }
+    
+    void Normalize() {
+      int counter;
+      int length = myDigitsNumber;
+      
+      for (counter = length - 1; counter >= 0; --counter) {
+        if (GetDigit(counter) != 0) {
+          break;
+        }
+        --myDigitsNumber;
+      }
+      
+      if (counter < 0) {
+        myDigitsNumber = 1;
+        mySign = positive;
+      }
+    }
+    
+  public:
+    BigInt(): 
+    mySign(positive),
+    myDigits(1, '0'),
+    myDigitsNumber(1)
+    {}
+
+    BigInt(int number): 
+    mySign(positive),
+    myDigits(1, '0'),
+    myDigitsNumber(0)
+    {
       if (number < 0) {
         mySign = negative;
         number *= -1;
@@ -56,45 +103,52 @@ class BigInt {
 
     }
 
-    BigInt(const string& inputString) {
-      mySign(positive);
-      myDigits(inputString.length(), '0');
-      myDigitsNumber(0);
+    BigInt(const string& inputString): 
+    mySign(positive),
+    myDigits(inputString.length(), '0'),
+    myDigitsNumber(0)
+    {
       int numberIndex;
       int limit = 0;
 
-      if (inputString.size()) == 0) {
+      if (inputString.size() == 0) {
         myDigits.resize(1);
         AddDigit(0);
         return;
       }
 
-      if (s[0] == '-') {
+      if (inputString[0] == '-') {
         mySign = negative;
         limit = 1;
       }
 
-      if (s[0] == '+') {
+      if (inputString[0] == '+') {
         limit = 1;
       }
 
-      for (numberIndex = inputString.size(); numberIndex >= limit; --k) {
-        AddDigit(s[k] - '0');
+      for (numberIndex = inputString.size() - 1; numberIndex >= limit; --numberIndex) {
+        AddDigit(inputString[numberIndex] - '0');
       }
+      
+      Normalize();
     }
     
-    istream& operator>> (istream& in, BigInt& number) {
-      string line;
-      in >> line;
-      number = BigInt(line);
-      return line;
+    void Print(ostream& output) {
+      output << ToString();
     }
-
-    /*void Print(ostream& out) {
-      int len = numDigits
-    }*/
-
 };
+
+ostream& operator <<(ostream& stream, BigInt& number) {
+  number.Print(stream);
+  return stream;
+}
+
+istream& operator>> (istream& in, BigInt& number) {
+  string line;
+  in >> line;
+  number = BigInt(line);
+  return in;
+}
 
 int main() {
   int num = 5;
